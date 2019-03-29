59251c7696c037e8a9fcdaf1e8ec8c5af4121428
rodipm <snnlion@gmai.com>
2019-03-29 20:04:09 -0300

    Added individual user directories

--- compile.sh
+++ compile.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-#########################
-# Usage:
-# watch.sh  <file>
-#########################
-inotifywait -m -e moved_to,create . | 
-	while read path event file; do
-		if [ "$file" == "main.cpp" ] || [ "$file" == "Assembler.cpp" ] || [ "$file" == "VM.cpp" ] || [ "$file" == "loader.asm" ] || [ "$file" == "test.asm" ] ; then
-			clear
-			#g++ -o ${1:0:(-4)} $1
-			g++ -o main main.cpp Assembler.cpp VM.cpp
-			./main
-		fi
-	done	
+g++ -o SisProg ./src/CLI.cpp ./src/Assembler.cpp ./src/VM.cpp
+./SisProg
